6833609ebf5d0a5573ae8a1e74ec44957a3d6c6f
Abraao Neves <abraaoborgesneves@gmail.com>
2020-06-21 18:38:38 -0300

    Class de tratamento de Troco (Exchange)
    Construcao inicial de classe responsavel por controle do troco considerando valores aplicados em pagamento.
    
    Controle comportamento de troco valido ou nao.

---
+++ src/main/java/br/com/abneves/exchange/domain/Exchange.java
@@ -0,0 +1,38 @@
+package br.com.abneves.exchange.domain;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Exchange
+ *
+ * @author Abraao Neves
+ * @version 0.0.1 21-06-2020
+ */
+public class Exchange {
+
+    private final Payment payment;
+
+    Exchange(Payment payment) {
+        this.payment = payment;
+    }
+
+    public static Exchange of(final Payment payment) {
+        return new Exchange(payment);
+    }
+
+    public Integer getExchange() {
+        if (payment.isAllowedToExchange()) {
+            return payment.getTotalReceived() - payment.getProductsValue();
+        }
+
+        throw new InvalidPaymentForExchange("Invalid value for exchange.");
+    }
+
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public static class InvalidPaymentForExchange extends RuntimeException {
+        public InvalidPaymentForExchange(String message) {
+            super(message);
+        }
+    }
+}
---
+++ src/test/java/br/com/abneves/exchange/domain/ExchangeTest.java
@@ -0,0 +1,43 @@
+package br.com.abneves.exchange.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExchangeTest {
+
+    @Test
+    public void shouldBeValidateRightExchange() {
+        final var customerPaymentValue = 100;
+        final var productValue = 90;
+
+        final var payment = Payment.of(1L, productValue, customerPaymentValue);
+        final var exchange = Exchange.of(payment);
+
+        Assertions.assertTrue(payment.isAllowedToExchange());
+        Assertions.assertEquals(10, exchange.getExchange());
+    }
+
+    @Test
+    public void shouldBeValidateRightExchangeWithSameProductValueAndCustomerValueReceive() {
+        final var customerPaymentValue = 100;
+        final var productValue = 100;
+
+        final var payment = Payment.of(1L, productValue, customerPaymentValue);
+        final var exchange = Exchange.of(payment);
+
+        Assertions.assertTrue(payment.isAllowedToExchange());
+        Assertions.assertEquals(0, exchange.getExchange());
+    }
+
+    @Test
+    public void shouldBeThrowInvalidCustomerPaymentValueForExchange() {
+        final var customerPaymentValue = 90;
+        final var productValue = 100;
+
+        final var payment = Payment.of(1L, productValue, customerPaymentValue);
+        final var exchange = Exchange.of(payment);
+
+
+        Assertions.assertThrows(Exchange.InvalidPaymentForExchange.class, exchange::getExchange);
+    }
+}
