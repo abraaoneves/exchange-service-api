5dc243e45ee5c360af4252c999309e7ebbfb3c41
Abraao Neves <abraaoborgesneves@gmail.com>
2020-06-23 02:42:27 -0300

    Removendo this para manter padrao em classe.

--- src/main/java/br/com/abneves/exchange/controllers/decorators/PaymentPageDecorator.java
+++ src/main/java/br/com/abneves/exchange/controllers/decorators/PaymentPageDecorator.java
@@ -1,21 +1,19 @@
 package br.com.abneves.exchange.controllers.decorators;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
 
+@AllArgsConstructor
 public class PaymentPageDecorator<T> {
 
     private final Page<T> page;
 
-    public PaymentPageDecorator(Page<T> page) {
-        this.page = page;
-    }
-
     @JsonProperty("payments")
     public List<T> getContent() {
-        return this.page.getContent();
+        return page.getContent();
     }
 
     public int getTotalPages() {
