3ac2f630d43d19f0cb6feee483c6337982ed25bf
Abraao Neves <abraaoborgesneves@gmail.com>
2020-06-23 01:41:11 -0300

    Implementacao Basica de Payment Service
    - Criando interacao com base de dados para Tests de integracao.

--- src/main/java/br/com/abneves/exchange/domain/services/implementation/PaymentServiceImpl.java
+++ src/main/java/br/com/abneves/exchange/domain/services/implementation/PaymentServiceImpl.java
@@ -1,17 +1,36 @@
 package br.com.abneves.exchange.domain.services.implementation;
 
 import br.com.abneves.exchange.domain.Payment;
+import br.com.abneves.exchange.domain.repositories.PayerRepository;
 import br.com.abneves.exchange.domain.services.PaymentService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.Collections;
 import java.util.List;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
+    private final PayerRepository repository;
+
+    public PaymentServiceImpl(PayerRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public List<Payment> listByPayer(Long payerId) {
-        return Collections.emptyList();
+        final var payer = repository
+                .findById(payerId)
+                .orElseThrow(() -> new PayerNotFoundException("Payer not found."));
+
+        return payer.getPayments();
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public static class PayerNotFoundException extends RuntimeException {
+        public PayerNotFoundException(String message) {
+            super(message);
+        }
     }
 }
