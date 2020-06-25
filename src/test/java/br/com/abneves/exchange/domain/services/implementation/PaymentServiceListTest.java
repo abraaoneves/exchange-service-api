04e77cb0422e5c9bf2fdf02e88fd3426b3750f72
Abraao Neves <abraaoborgesneves@gmail.com>
2020-06-25 20:24:33 -0300

    Melhorando test de listagem
    - Para melhorar o test foi alterado o nome e divida a responsabilidade.
    - Adicionado novo test para lista vazia.

--- src/test/java/br/com/abneves/exchange/domain/services/implementation/PaymentServiceListTest.java
+++ src/test/java/br/com/abneves/exchange/domain/services/implementation/PaymentServiceListTest.java
@@ -3,21 +3,25 @@ package br.com.abneves.exchange.domain.services.implementation;
 import br.com.abneves.exchange.creators.PaymentCreator;
 import br.com.abneves.exchange.domain.Payment;
 import br.com.abneves.exchange.domain.repositories.PaymentRepository;
+import br.com.abneves.exchange.domain.services.PayerService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.when;
 
 @ExtendWith(SpringExtension.class)
-public class PaymentServiceTest {
+public class PaymentServiceListTest {
 
     @InjectMocks
     private PaymentServiceImpl paymentService;
@@ -31,6 +35,7 @@ public class PaymentServiceTest {
     public void setUp() {
         final var payments = PaymentCreator.createPagesOfPaymentList();
         when(repository.findAllByPayerPayerId(1L, PAGEABLE)).thenReturn(payments);
+        when(repository.findAllByPayerPayerId(2L, PAGEABLE)).thenReturn(new PageImpl<>(Collections.emptyList()));
     }
 
     @Test
@@ -44,6 +49,16 @@ public class PaymentServiceTest {
                 .first()
                 .extracting(Payment::getPaymentId)
                 .isEqualTo(1L);
+    }
+
+    @Test
+    @DisplayName("Return a empty list of payments for invalid payer id")
+    public void listPaymentsByPayer_whenInvalidPayerIsInformed_emptyListWasReturn() {
+        final var payments = paymentService.listByPayer(2L, PAGEABLE);
+
+        assertThat(payments.getTotalElements()).isEqualTo(0);
+        assertThat(payments.getContent())
+                .isEmpty();
     }
 
 }
