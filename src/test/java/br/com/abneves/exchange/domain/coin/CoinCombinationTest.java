55f59264998eb0b620493bec7004cd8ab8c46e3a
Abraao Neves <abraaoborgesneves@gmail.com>
2020-06-25 03:39:37 -0300

    Domain de Combinacao de moedas.
    - Domain responsavel por controlar combinacao de meodas, criando uma combinacao mininma utilizando do maior para o menor, desta forma reduzindo o numero de interacoes.

---
+++ src/test/java/br/com/abneves/exchange/domain/coin/CoinCombinationTest.java
@@ -0,0 +1,69 @@
+package br.com.abneves.exchange.domain.coin;
+
+import org.junit.jupiter.api.*;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CoinCombinationTest {
+
+    @Test
+    @Order(1)
+    @Timeout(value = 5, unit = TimeUnit.MILLISECONDS)
+    @DisplayName("Return a list of 3 combinations for valid exchange value")
+    public void listOfCombination_shouldBeReturnListOfThreeCombinationsOfCoins_whenValidExchangeValueIsUsed() {
+        final var coinCombination = CoinCombination.of();
+        coinCombination.findCoinsCombinations(10);
+
+        assertThat(coinCombination.getCombinations())
+                .hasSize(3)
+                .first()
+                .extracting(List::size).isEqualTo(2);
+    }
+
+    @Test
+    @Order(2)
+    @Timeout(value = 150, unit = TimeUnit.MILLISECONDS)
+    @DisplayName("Return a list of 3 combinations for valid and big exchange value")
+    public void listOfCombination_shouldBeReturnListOfThreeCombinationsOfCoins_whenValidAndBigExchangeValueIsUsed() {
+        final var coinCombination = CoinCombination.of();
+        coinCombination.findCoinsCombinations(500);
+
+        assertThat(coinCombination.getCombinations())
+                .hasSize(3);
+    }
+
+    @Test
+    @Order(3)
+    @Timeout(value = 1000, unit = TimeUnit.MILLISECONDS)
+    @DisplayName("Return a list of 3 combinations for valid and biggest exchange value")
+    public void listOfCombination_shouldBeReturnListOfThreeCombinationsOfCoins_whenValidAndBiggestExchangeValueIsUsed() {
+        final var coinCombination = CoinCombination.of();
+        coinCombination.findCoinsCombinations(849);
+
+        assertThat(coinCombination.getCombinations())
+                .hasSize(3);
+    }
+
+    @Test
+    @Order(4)
+    @Timeout(value = 10, unit = TimeUnit.MILLISECONDS)
+    @DisplayName("Should throw exception when ZERO value for exchange is set.")
+    public void shouldThrowException_whenZeroExchangeValueIsSet() {
+        final var coinCombination = CoinCombination.of();
+
+        Assertions.assertThrows(CoinCombination.CoinExchangeCombinationInvalid.class, () -> coinCombination.findCoinsCombinations(0));
+    }
+
+    @Test
+    @Order(5)
+    @Timeout(value = 10, unit = TimeUnit.MILLISECONDS)
+    @DisplayName("Should throw exception when BIGGER value for exchange is set.")
+    public void shouldThrowException_whenBiggerExchangeValueIsSet() {
+        final var coinCombination = CoinCombination.of();
+
+        Assertions.assertThrows(CoinCombination.CoinExchangeCombinationInvalid.class, () -> coinCombination.findCoinsCombinations(850));
+    }
+}
