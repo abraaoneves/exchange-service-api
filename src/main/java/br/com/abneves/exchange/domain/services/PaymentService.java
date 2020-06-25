e02c36a9abc11c531181edb804bb3c6a6144f10e
Abraao Neves <abraaoborgesneves@gmail.com>
2020-06-25 03:45:28 -0300

    Adicionando metodo de pagamento
    - Responsavel por gerar um novo pagamento e registros de pagamento de um pagador existente no banco de dados.

--- src/main/java/br/com/abneves/exchange/domain/services/PaymentService.java
+++ src/main/java/br/com/abneves/exchange/domain/services/PaymentService.java
@@ -1,9 +1,11 @@
 package br.com.abneves.exchange.domain.services;
 
+import br.com.abneves.exchange.controllers.vos.PaymentRequest;
 import br.com.abneves.exchange.domain.Payment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface PaymentService {
     Page<Payment> listByPayer(final Long payerId, final Pageable pageable);
+    Payment pay(final Long payerId, final PaymentRequest paymentRequest);
 }
--- src/main/java/br/com/abneves/exchange/domain/services/implementation/PaymentServiceImpl.java
+++ src/main/java/br/com/abneves/exchange/domain/services/implementation/PaymentServiceImpl.java
@@ -1,7 +1,10 @@
 package br.com.abneves.exchange.domain.services.implementation;
 
+import br.com.abneves.exchange.controllers.vos.PaymentRequest;
+import br.com.abneves.exchange.domain.Payer;
 import br.com.abneves.exchange.domain.Payment;
 import br.com.abneves.exchange.domain.repositories.PaymentRepository;
+import br.com.abneves.exchange.domain.services.PayerService;
 import br.com.abneves.exchange.domain.services.PaymentService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -10,12 +13,29 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class PaymentServiceImpl implements PaymentService {
+class PaymentServiceImpl implements PaymentService {
 
     private final PaymentRepository repository;
+    private final PayerService payerService;
 
     @Override
-    public Page<Payment> listByPayer(Long payerId, Pageable pageable) {
+    public Page<Payment> listByPayer(final Long payerId, final Pageable pageable) {
         return repository.findAllByPayerPayerId(payerId, pageable);
+    }
+
+    @Override
+    public Payment pay(final Long payerId, final PaymentRequest paymentRequest) {
+        final var payer = payerService.getById(payerId);
+        return save(payer, paymentRequest.getProductsValue(), paymentRequest.getTotalReceived());
+    }
+
+    private Payment save(final Payer payer, final Integer productsValue, final Integer totalReceived) {
+        final var payment = Payment.builder()
+                .payer(payer)
+                .productsValue(productsValue)
+                .totalReceived(totalReceived)
+                .build();
+
+        return repository.save(payment);
     }
 }
